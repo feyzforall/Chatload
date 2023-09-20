cd48844af2f9908260ca89bb2e57ecb7b09f01bf
Feyzullah Kodat <feyzkodat@gmail.com>
2023-09-20 12:20:55 +0300

    fixed appnameview close #4

--- lib/view/common_widgets/app_name_view.dart
+++ lib/view/common_widgets/app_name_view.dart
@@ -18,36 +18,28 @@ class AppNameView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // chat of chatload
-        Text(
-          AppStrings.chat,
-          style: appNameType == AppNameType.body
-              ? Theme.of(context).textTheme.displaySmall?.copyWith(
-                    color: AppColors.frenchPass,
-                    fontWeight: FontWeight.bold,
-                  )
-              : Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    color: AppColors.frenchPass,
-                    fontWeight: FontWeight.bold,
-                  ),
-        ),
-        // load of chatload
-        Text(
-          AppStrings.load,
-          style: appNameType == AppNameType.body
-              ? Theme.of(context).textTheme.displaySmall?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )
-              : Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-        ),
-      ],
+    return RichText(
+      text: TextSpan(
+        text: AppStrings.chat,
+        style: appNameType == AppNameType.body
+            ? Theme.of(context).textTheme.displaySmall?.copyWith(
+                  color: AppColors.frenchPass,
+                  fontWeight: FontWeight.bold,
+                )
+            : Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  color: AppColors.frenchPass,
+                ),
+        children: const [
+          TextSpan(
+            text: AppStrings.load,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
+
+    // );
   }
 }
